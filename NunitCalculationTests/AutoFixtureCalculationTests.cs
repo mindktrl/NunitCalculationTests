d0b646a33647725f8572bff2734481ffa737b284
mindktrl <emersion.space@gmail.com>
2023-04-27 11:42:05 +0100

    extends calcuation class

--- NunitCalculationTests/AutoFixtureCalculationTests.cs
+++ NunitCalculationTests/AutoFixtureCalculationTests.cs
@@ -74,5 +74,88 @@ namespace NunitCalculationTests
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [Test]
+        public void TestModulo()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            int a = fixture.Create<int>();
+            int b = fixture.Create<int>();
+            if (b == 0)
+            {
+                b = 1; // Avoid division by zero error in Arrange phase
+            }
+            int expectedResult = a % b;
+            var calculator = new Calculator();
+
+            // Act
+            int actualResult = calculator.Modulo(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestPower()
+        {
+            // Arrange
+            double a = _fixture.Create<double>();
+            double b = _fixture.Create<double>();
+            double expectedResult = Math.Pow(a, b);
+
+            // Act
+            double actualResult = _calculator.Power(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestSquareRoot()
+        {
+            // Arrange
+            double a = _fixture.Create<double>();
+            if (a < 0)
+            {
+                Assert.Throws<ArgumentException>(() => _calculator.SquareRoot(a));
+            }
+            else
+            {
+                double expectedResult = Math.Sqrt(a);
+
+                // Act
+                double actualResult = _calculator.SquareRoot(a);
+
+                // Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        [Test]
+        public void TestFactorial()
+        {
+            // Arrange
+            int a = _fixture.Create<int>();
+            if (a < 0)
+            {
+                Assert.Throws<ArgumentException>(() => _calculator.Factorial(a));
+            }
+            else
+            {
+                int expectedResult = 1;
+                for (int i = a; i > 0; i--)
+                {
+                    expectedResult *= i;
+                }
+
+                // Act
+                int actualResult = _calculator.Factorial(a);
+
+                // Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
     }
 }
--- NunitCalculationTests/Calculator.cs
+++ NunitCalculationTests/Calculator.cs
@@ -19,7 +19,48 @@ namespace NunitCalculationTests
 
         public int Divide(int a, int b)
         {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
             return a / b;
+        }
+
+        public int Modulo(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Cannot take modulo by zero.");
+            }
+            return a % b;
+        }
+
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double SquareRoot(double a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot take square root of a negative number.");
+            }
+            return Math.Sqrt(a);
+        }
+
+        public int Factorial(int a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot take factorial of a negative number.");
+            }
+            int result = 1;
+            for (int i = a; i > 0; i--)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
